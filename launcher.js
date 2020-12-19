68ce7939f4d3cde0f3c94113b00f8cc11e2ea023
tanvidawra19 <74242965+tanvidawra19@users.noreply.github.com>
2020-12-19 12:57:48 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,33 @@
+class launcher{
+    constructor(body , anchor){
+        var options={
+            bodyA : body,
+            pointB : anchor,
+            stiffness : 0.04,
+            length : 10
+        }
+        this.bodyA = body;
+        this.pointB = anchor;
+        this.launcher = Constraint.create(options);
+        World.add (world , this.launcher)
+    }
+
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        if(this.launcher.bodyA){
+        var pointA = this.bodyA.position ;
+        var pointB = this.pointB ;
+
+        strokeWeight(4);
+        line(pointA.x , pointA.y , pointB.x , pointB.y);
+        }
+    }
+}
+
